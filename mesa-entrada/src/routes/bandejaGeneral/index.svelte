a7d354988ed117bf7b9a8804867f743f073a3de5
cristianAlvarezInfo <cris122.lp@gmail.com>
2021-12-15 19:21:54 -0300

    Agregado estilo a listado bandeja gral mesa entrada

--- mesa-entrada/src/routes/bandejaGeneral/index.svelte
+++ mesa-entrada/src/routes/bandejaGeneral/index.svelte
@@ -51,6 +51,7 @@
         Trayendo sociedad anonimas. Espere :)
     {:then}
         {#if segments && segments.length}
+        <!--
         <ul style="display: flex; flex-direction: column;  list-style:none;">
         {#each segments as sociedad}
                 <br>
@@ -59,6 +60,39 @@
                 <li style="margin-left: 10px"><button on:click={ apropiarse(sociedad.task_id) }>Apropiarse</button></li>
         {/each}
         </ul>
+        -->
+        <div class="container-table">
+          <table>
+            <thead>
+              <td>Nombre Sociedad</td>
+              <td>Email Apoderado</td>
+              <td>Fecha de pedido</td>
+              <td>Domicilio legal</td>
+              <td>Domicilio Real</td>
+              <td>Estatuto</td>
+              <td>Opciones</td>
+            </thead>
+            <tbody>
+              {#each segments as sociedad}
+               
+                <tr>
+                  <td>{ sociedad.data.nombre }</td>
+                  <td>{ sociedad.data.emailApoderado }</td>
+                  <td>{ sociedad.data.nombre }</td>
+                  <td>{ sociedad.data.fechaCreacion }</td>
+                  <td>{ sociedad.data.domicilioReal }</td>
+                  <td><a href={`http://localhost:8000/pdf_viewer/${sociedad.data.id}/`}>Ver Estatuto</a></td>
+                  <td>
+                    <div class="inline-container">
+                      <div class="option-btn btn-apropiarse" style="margin-right: 10px" on:click={ apropiarse(sociedad.task_id) }> Apropiarse </div>
+                      <div class="option-btn btn-detalle"> Detalle </div>
+                    </div>
+                  </td>
+                </tr>
+              {/each}
+            </tbody>
+          </table>
+        </div>
         {:else}
         <div>
             No hay tareas pendientes
@@ -67,3 +101,39 @@
     {/await}
 </div>
 
+
+
+<style>
+  .inline-container {
+    display: flex;
+    flex-direction: row;
+  }
+  .container-table{
+    display: flex;
+    justify-content: center;
+  }
+  *{
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  td{
+    width: 200px;
+  }
+  tr{
+    height: 70px;
+  }
+  .option-btn {
+    width: 100px;
+    height: 30px;
+    color: white;
+    padding-top: 10px;
+    border-radius: 6px;
+    text-align: center;
+  }
+  .btn-apropiarse {
+    background-color: green;
+  }
+  .btn-detalle {
+    background-color: blue;
+  }
+
+</style>
