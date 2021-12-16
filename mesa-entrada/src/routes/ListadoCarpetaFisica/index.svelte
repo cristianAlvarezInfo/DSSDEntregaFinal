90dd58d3e263f06e49e0400d458349a3151bac62
cristianAlvarezInfo <cris122.lp@gmail.com>
2021-12-16 18:52:47 -0300

    hechos todos los listados

--- mesa-entrada/src/routes/ListadoCarpetaFisica/index.svelte
+++ mesa-entrada/src/routes/ListadoCarpetaFisica/index.svelte
@@ -1,4 +1,5 @@
 <script>
+    import  DetailModal  from '$component/DetailModal.svelte';
     import Handlebars from 'handlebars';
     import pdf from 'html-pdf';
     import path from 'path'
@@ -40,7 +41,7 @@
       return Promise.resolve();
     };
 
-    const eliminarSociedadApropiada = (taskID) => {
+    const eliminarSociedadCarpeta = (taskID) => {
         segments = segments.filter(e => {
             e.task_id != taskID
         })
@@ -58,6 +59,8 @@
 
 
     const exportToPdf = async (sociedad) => {
+        eliminarSociedadCarpeta(sociedad.task_id);
+
         const template = `
             Nombre {{nombre}} 
             Fecha Creacion {{fecha_creacion}} 
@@ -86,10 +89,72 @@
         
         await fetch('http://localhost:8000/api/save_carpeta_fisica/', opts)
         await fetch(URL, { method:"POST", body: setBody("mensajeMesaInfo",email,"java.lang.String",sociedad.case_id) })
-        await updateTaskState(sociedad.task_id)
+        await updateTaskState(sociedad.task_id);
+
     }
+
+    function dropSociedadCarpeta(sociedad){
+        return segments.filter(e => e.data.id == sociedad.data.id);
+    }
+
+    function closeModal(){ 
+        selectedModal = -1;
+      }
+
+      let selectedModal = -1;
 </script>
 
+
+<div>
+    {#await getTasks() }
+        Trayendo sociedad anonimas. Espere :)
+    {:then}
+        {#if segments && segments.length}
+        <div class="container-table">
+          <table>
+            <thead>
+              <td>Nombre Sociedad</td>
+              <td>Email Apoderado</td>
+              <td>Fecha de pedido</td>
+              <td>Domicilio legal</td>
+              <td>Domicilio Real</td>
+              <td>Estatuto</td>
+              <td>Opciones</td>
+            </thead>
+            <tbody>
+              {#each segments as sociedad, i}
+                <tr>
+                  <td>{ sociedad.data.nombre }</td>
+                  <td>{ sociedad.data.emailApoderado }</td>
+                  <td>{ sociedad.data.fechaCreacion }</td>
+                  <td>{ sociedad.data.domicilioLegal }</td>
+                  <td>{ sociedad.data.domicilioReal }</td>
+                  <td><a href={`http://localhost:8000/pdf_viewer/${sociedad.data.id}/`} target="_blank">Ver Estatuto</a></td>
+                  <td>
+                    <div class="inline-container">
+                      <div class="option-btn btn-apropiarse" style="margin-right: 10px" on:click={ exportToPdf(sociedad) }> 
+                          Generar carpeta fisica 
+                      </div>
+                      <div class="option-btn btn-detalle" on:click={() => selectedModal = i}> Detalle </div>
+                      {#if selectedModal == i}
+                        <DetailModal {sociedad} bind:selectedModal={selectedModal} />
+                      {/if}
+                    </div>
+                  </td>
+                </tr>
+              {/each}
+            </tbody>
+          </table>
+        </div>
+        {:else}
+        <div>
+            No hay tareas pendientes
+        </div> 
+        {/if}
+    {/await}
+</div>
+
+<!--
 <div>
     {#await getTasks() }
         Trayendo sociedad anonimas. Espere :)
@@ -111,3 +176,58 @@
         {/if}
     {/await}
 </div>
+-->
+
+
+
+<style>
+    .inline-container {
+      display: flex;
+      flex-direction: row;
+    }
+    .column-container {
+      display: flex;
+      flex-direction: column;
+    }
+    .container-table{
+      display: flex;
+      justify-content: center;
+    }
+    *{
+      font-family: Arial, Helvetica, sans-serif;
+    }
+    td{
+      width: 200px;
+    }
+    tr{
+      height: 70px;
+    }
+    .inline-end {
+      display: flex;
+      justify-content: end;
+    }
+    .modal-title {
+      font-weight: 700;
+      font-size: 20px;
+    }
+    .modal-text{
+      font-size: 13;
+    }
+    .option-btn {
+      width: 100px;
+      height: 30px;
+      color: white;
+      padding-top: 10px;
+      border-radius: 6px;
+      text-align: center;
+    }
+    .btn-apropiarse {   
+      font-size: 11px !important;
+      background-color: green;
+    }
+    .btn-detalle {
+      background-color: blue;
+    }
+  
+  </style>
+  
